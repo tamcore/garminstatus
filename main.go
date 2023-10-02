ba63417b92a87452b9940d70ece14c5797a1bdd5
Philipp Born <git@pborn.eu>
2023-10-02 18:20:33 +0200

    add http server

--- main.go
+++ main.go
@@ -5,22 +5,64 @@ import (
 	"fmt"
 	"log"
 
+	"flag"
+
+	"net/http"
+
 	"github.com/tamcore/garminstatus/pkg/garminstatus"
 )
 
+var (
+	serveHTTP bool
+	httpPort  int
+)
+
+func init() {
+	flag.BoolVar(&serveHTTP, "http", false, "Start an HTTP server to serve the JSON data")
+	flag.IntVar(&httpPort, "port", 8080, "HTTP server port")
+	flag.Parse()
+}
+
 func main() {
-	// Fetch Garmin service status
-	status, err := garminstatus.FetchStatus()
-	if err != nil {
-		log.Fatal(err)
-	}
+	if serveHTTP {
+		// Start an HTTP server if the -http flag is provided
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			// Fetch Garmin service status on each request when serving via HTTP
+			status, err := garminstatus.FetchStatus()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
 
-	// Convert the status to JSON
-	jsonData, err := json.Marshal(status)
-	if err != nil {
-		log.Fatal(err)
-	}
+			// Convert the status to JSON
+			jsonData, err := json.Marshal(status)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
 
-	// Print the JSON data
-	fmt.Println(string(jsonData))
+			w.Header().Set("Content-Type", "application/json")
+			w.Write(jsonData)
+		})
+
+		fmt.Println("Starting HTTP server on port", httpPort)
+		err := http.ListenAndServe(fmt.Sprintf(":%d", httpPort), nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		// Fetch Garmin service status and output to stdout when not serving via HTTP
+		status, err := garminstatus.FetchStatus()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Convert the status to JSON
+		jsonData, err := json.Marshal(status)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(string(jsonData))
+	}
 }
