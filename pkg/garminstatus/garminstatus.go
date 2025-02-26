a08b74b6cb2b28b06b1328cbbddc71ca400d5c31
Philipp B. <git@pborn.eu>
2025-02-26 20:20:16 +0100

    fix exitAfterDefer (#30)
    * fix exitAfterDefer
    
    * fix

--- pkg/garminstatus/garminstatus.go
+++ pkg/garminstatus/garminstatus.go
@@ -2,7 +2,7 @@ package garminstatus
 
 import (
 	"context"
-	"log"
+	"fmt"
 	"net/http"
 	"strings"
 	"time"
@@ -51,16 +51,18 @@ func FetchStatus() (GarminStatus, error) {
 	// Fetch the HTML content from the URL
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, GarminConnectStatusURI, http.NoBody)
 	if err != nil {
-		log.Fatal(err)
+		return GarminStatus{}, fmt.Errorf("error creating request: %w", err)
 	}
 	resp, err := httpClient.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		return GarminStatus{}, fmt.Errorf("error during http request: %w", err)
 	}
-	defer resp.Body.Close()
+	defer func() {
+		_ = resp.Body.Close()
+	}()
 
 	if resp.StatusCode != http.StatusOK {
-		return GarminStatus{}, err
+		return GarminStatus{}, fmt.Errorf("invalid response status code: %d", resp.StatusCode)
 	}
 
 	// Parse the HTML content with goquery
